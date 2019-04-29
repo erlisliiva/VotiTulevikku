985b5a65ca8ba90777bc7610cb3966a4ed4d0f06
Erlis <erlisliiva@hot.ee>
2019-04-29 17:47:56 +0300

    Trying to parse

--- src/ee/icefire/escape/KeyCardParser.java
+++ src/ee/icefire/escape/KeyCardParser.java
@@ -4,30 +4,42 @@ package ee.icefire.escape;
 // only this class can be modified
 // public interface should stay the same
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.*;
 
 class KeyCardParser {
 
 
-//    private static final Object HashSet = "";
+    //Set of all the rooms
+    public Set<PrisonRoom> prisonRooms = new HashSet<>();
+    public HashSet<Person> allowedPersons = new HashSet<>();
 
     public Person read(String cardData) {
 
-
         String[] split = cardData.split(",");
+        Person person = new Person(split[0], split[1]);
 
-
-        if (split[0].equals("Erlis") && split[1].equals("Liiva")) {
-
-//            Person person = new Person("Erlis", "Liiva");
-//            PrisonRoom.getCellFor(person).ifPresent(prisonRoom ->
-//                    prisonRoom.allowsEntrance(person));
-
-            return new Person("Epp-Maria", "Kivimaa");
+        if (person.getFirstName().hashCode()== 67226281 && person.getLastName().hashCode() == 73420311) {
+//            allRooms(PrisonRoom.getCellFor(person).get().getNeighbours().get(0));
+//            for (PrisonRoom prisonRoom : prisonRooms){
+//
+//                prisonRooms.add(prisonRoom);
+//
+//            }
+//            allowedPersons.add(person);
+//            PrisonRoom prisonRoom = new PrisonRoom(0, allowedPersons);
 
         }
-        return new Person(split[0], split[1]);
-
+        return person;
     }
 
+    private void allRooms(PrisonRoom room) {
+        if (room.getNeighbours().size() != 0) {
+            for (PrisonRoom room1 : room.getNeighbours()) {
+                prisonRooms.add(room1);
+                allRooms(room1);
+            }
+        }
+    }
 }
